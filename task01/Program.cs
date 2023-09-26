f156f6a7ba526418ebe44a2c238e9f07674a8fc7
Alexey Privalov <privalov_a90@bk.ru>
2023-09-26 11:50:48 +0300

    add task01

---
+++ task01/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите первое целое число");
+int numberA = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе целое число");
+
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+if (numberB * numberB == numberA)
+{
+    Console.WriteLine("Первое число является квадратом второго");
+}
+else
+{
+    Console.WriteLine("Первое число не является квадратом второго");
+}
