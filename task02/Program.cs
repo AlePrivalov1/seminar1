8282127c9697719bfde049bd9f5d790b0eb180d0
Alexey Privalov <privalov_a90@bk.ru>
2023-09-26 11:51:29 +0300

    add task02

---
+++ task02/Program.cs
@@ -0,0 +1,34 @@
+﻿Console.WriteLine("Введите номер дня недели");
+string day = Console.ReadLine();
+if (day == "1")
+{
+    Console.WriteLine("понедельник");
+}
+else if (day == "2")
+{
+    Console.WriteLine("вторник");
+}
+else if (day == "3")
+{
+    Console.WriteLine("среда");
+}
+else if (day == "4")
+{
+    Console.WriteLine("четверг");
+}
+else if (day == "5")
+{
+    Console.WriteLine("пятница");
+}
+else if (day == "6")
+{
+    Console.WriteLine("суббота");
+}
+else if (day == "7")
+{
+    Console.WriteLine("воскресение");
+}
+else 
+{
+    Console.WriteLine("Некоректный Ввод");
+}
