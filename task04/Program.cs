8df9f61c0313b955b7279087c0045e94a66883ae
Alexey Privalov <privalov_a90@bk.ru>
2023-09-26 11:54:21 +0300

    add task04

---
+++ task04/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите трехзначное число:");
+int nemberA = Convert.ToInt32(Console.ReadLine());
+
+if (nemberA < 0 ) nemberA *= -1;
+
+if (nemberA >= 100 && nemberA <= 999);
+{
+    int lastDigit = nemberA % 10;
+
+    Console.Write($"последняя цифра числа -> {lastDigit}");
+}
+// else
+// {
+//     Console.WriteLine("некоректный ввод");
+// } 
+
+
