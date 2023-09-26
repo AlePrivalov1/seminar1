ed6ee1383fb1f476646261b24022c3abd694b0f2
Alexey Privalov <privalov_a90@bk.ru>
2023-09-26 11:53:55 +0300

    add task03

---
+++ task03/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите первое целое число");
+int numberA = Convert.ToInt32(Console.ReadLine());
+
+if (numberA > 0)
+{
+    int count = -numberA;
+    while (count <= numberA)
+    {
+        Console.Write($"{count}");
+        count += 1;
+    }
+}
+
+else
+{
+    Console.WriteLine("ошибка ввода!");
+}
