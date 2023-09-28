63114bc622a816fd400231bc89b5daa9db95e4c6
Alexey Privalov <privalov_a90@bk.ru>
2023-09-28 21:13:59 +0300

    add homework task12

---
+++ task12/Program.cs
@@ -0,0 +1,27 @@
+﻿// напишите программу каторая впринимает 2 числа если кратно второму, если не кратно остаток
+int GetUserInput(string str)
+{
+    Console.Write(str);
+    int num = Convert.ToInt32(Console.ReadLine());
+    return num;
+}
+
+
+int IGetRemaider(int num1, int num2)
+{
+     return num1 % num2;
+    
+}
+
+int firstNum = GetUserInput("Ведите первое число");
+int secondNum = GetUserInput("Ведите второе число");
+int rem = IGetRemaider(firstNum , secondNum);
+
+if (rem == 0)
+{
+    Console.WriteLine($" число {firstNum} кратно {secondNum}");
+}
+else
+{
+    Console.WriteLine($" число {firstNum} не кратно {secondNum}  остаток {rem}");
+}
