16fc6e7caef61ac6bf6a1188aa7c9c936e803a9a
Alexey Privalov <privalov_a90@bk.ru>
2023-09-28 21:13:27 +0300

    add homework task11

---
+++ task11/Program.cs
@@ -0,0 +1,18 @@
+﻿// ввод трехзначное вывод двузначное
+int DeleteSecondDigit(int num)
+{
+    int firstDigit = num / 100;
+    int thrdDigit = num % 10;
+    return  firstDigit * 10 + thrdDigit; 
+}
+
+
+
+
+int numberA = new Random().Next(100,1000);
+Console.WriteLine(numberA);
+// int firstDigit = numberA / 100;
+// int thrdDigit = numberA % 10;
+// int result = firstDigit * 10 + thrdDigit;
+int result = DeleteSecondDigit(numberA);
+Console.WriteLine(result);
