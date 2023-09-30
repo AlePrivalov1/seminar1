a6c5791078dcc5d236428aa9f2e9bc204801b6c3
Alexey Privalov <privalov_a90@bk.ru>
2023-09-30 10:43:17 +0300

    add homework task28

---
+++ task28/Program.cs
@@ -0,0 +1,24 @@
+﻿// напишите прогграмму каторая принимает на вход число N 
+// и выдает произведение чисел от 1 до n
+// 4 -> 24
+// 5 - >120
+
+int Sumnumbers(int num)
+{
+    int sum = 1;
+    for (int i = 1; i <= num; i++)
+    {
+        checked
+        {
+            sum *= i;
+        }
+        
+    }
+    return sum;
+}
+Console.WriteLine("Введите целое положтелнок число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+
+int sumNumbers = Sumnumbers(number);
+Console.WriteLine($"произведение чисел от 1 до {number} = {sumNumbers}");
