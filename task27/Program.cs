c44d31ba3b8615eabb8768994e902b7dc50dae18
Alexey Privalov <privalov_a90@bk.ru>
2023-09-30 10:43:09 +0300

    add homework task27

---
+++ task27/Program.cs
@@ -0,0 +1,29 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+int GetUserInput(string msg)
+{
+    Console.WriteLine($"{msg}: ");
+    int num = Convert.ToInt32(Console.ReadLine());
+    return num;
+}
+
+int LenghtNum(int num)
+{
+    
+    int result = 0;
+    while (num > 0 )
+    {
+        result += num %10; //прибовляем остаток от деления последнюю цифру
+        num = num/10; //уменьшаем разряд
+    }
+    return result;
+}
+
+Console.WriteLine("Введите число ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+int sumNumbers = LenghtNum(numberB);
+Console.WriteLine($"{numberB} -> {sumNumbers}");
