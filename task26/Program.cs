4c25e1dae9b81c16031d9a88a0690552d8fabfc0
Alexey Privalov <privalov_a90@bk.ru>
2023-09-30 10:43:02 +0300

    add homework task26

---
+++ task26/Program.cs
@@ -0,0 +1,27 @@
+﻿// напишите програму каторая прингимает число 
+// и выдает количество чисел в числе
+// 456 -- > 3
+// 78 --> 2
+
+int GetUserInput(string msg)
+{
+    Console.WriteLine($"{msg}: ");
+    int num = Convert.ToInt32(Console.ReadLine());
+    return num;
+}
+int LenghtNum(int num)
+{
+    if (num == 0) return 1; 
+    int count = 0;
+    while (num != 0 )
+    {
+        count ++;
+        num = num/10;
+    }
+    return count;
+}
+
+int number = GetUserInput("enter a number");
+int lenghr = LenghtNum(number);
+
+Console.WriteLine($"{number} -> {lenghr}");
